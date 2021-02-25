41a6ddcbefff12609ef8e4474678497e97a58bf0
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-02-25 20:49:39 -0300

    7. Utilizando for , descubra qual o menor valor contido no array e imprima-o

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/7.js
@@ -1,18 +1,19 @@
 /*
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-let maior = 0
+
+let menor = numbers[6]  
 
 for (let index = 0; index < numbers.length; index++) {
-    if (numbers[index] > maior) {
-        maior = numbers[index]
+    if (numbers[index] < menor) {
+        menor = numbers[index]
     }
 }
 
-console.log(`O maior número do array é: ${maior}`)
+console.log(`O menor número do array é: ${menor}`)
 
 /*Para resolver esse exercício, utilizei esse site:
 https://www.clubedohardware.com.br/topic/1231893-como-achar-o-maior-e-menor-valor-em-um-vetor/
