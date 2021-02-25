357f54b16867abafedb4018b8d355c275e7fe0b2
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-02-25 20:52:51 -0300

    8. Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/8.js
@@ -0,0 +1,14 @@
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+8. Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27] 
+
+array = []
+
+for (let index = 0; index < 25; index++) {
+    array[index] = index+1
+}
+
+console.log(array)
