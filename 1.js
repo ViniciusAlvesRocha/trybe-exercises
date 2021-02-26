1d16e161be62395cfc53cb99772d63b2f04a89d5
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-02-26 20:06:47 -0300

    1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n

---
+++ 1.js
@@ -0,0 +1,19 @@
+/*
+1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+*/
+
+let n = 5
+let line = []
+let asterisco = "*"
+
+if (n<=1) {
+    console.log("escolha valor maior do que 1!")
+}else {
+    for (let addAsterisco = 0; addAsterisco < n; addAsterisco++) {
+        line = line+asterisco
+    }
+    
+    for (let lines= 1; lines<=n; lines++) {
+        console.log(line)
+    }
+}
