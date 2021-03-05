65167861b04008ecb3e8e1a7bded53efca694c22
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-05 19:30:41 -0300

    Exercicio 1 Trabalho inicial com o array de dias

---
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,36 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+  ulDays = document.getElementById('days');
+  console.log(ulDays);
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let liDay = document.createElement('li');
+    liDay.setAttribute('class', 'day');
+    if (dezDaysList[i] === 24 || dezDaysList[i] === 31) {
+        liDay.setAttribute('class', 'day holiday')
+    }else if (dezDaysList[i] === 25) {
+        liDay.setAttribute('class', 'day holiday friday');
+    }else if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 11 || dezDaysList[i] === 18) {
+        liDay.setAttribute('class', 'day friday')
+    }
+    liDay.innerText = dezDaysList[i];
+    ulDays.appendChild(liDay)
+  }
+
