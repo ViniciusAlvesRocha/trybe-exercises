22bd5396f6fbf79d90e523bdb7dacc61a3748811
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-16 09:44:30 -0300

    2 - Criando formulario do ultimo emprego

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -26,3 +26,81 @@ window.onload = function() {
   statesComboBox.appendChild(option);
   }
 }
+
+buttonSubmit = document.querySelector('[type="submit"]');
+console.log(buttonSubmit);
+function verifyDay(day) {
+  if (day < 0 || day >=31) {
+    alert('dia inválido')
+  }
+}
+
+function verifyMonth(month) {
+  if (month < 0 || month >= 12) {
+    alert('mês inválido')
+  }
+}
+
+function verifyYear(year) {
+  if (year < 0) {
+    alert('ano inválido')
+  }
+}
+function submit(event) {
+  event.preventDefault();
+  inputInit = document.getElementById('id-inicio');
+  date = inputInit.value.split('/');
+  console.log(date);
+  verifyDay(date[0]);
+  verifyMonth(date[1]);
+  verifyYear(date[2]);
+}
+
+buttonSubmit.addEventListener('click', submit);
+inputInit = document.getElementById('id-inicio');
+function maskDate(el) {
+  date = el.target.value;
+  if (el.keyCode !== 8) {
+    if (date.length === 2) {
+      date = date.split();
+      date.splice(1, 0, '/');
+      date = date.join('');
+      inputInit.value = date;
+      /* console.log(date); */
+    } else if (date.length === 3) {
+      date = date.split();
+      date.splice(1, 0, '/');
+      date = date.join('');
+      inputInit.value = date;
+      /* console.log(date); */
+    }else if (date.length === 5) {
+      date = date.split();
+      date.splice(4, 0, '/');
+      date = date.join('');
+      inputInit.value = date;
+      /* console.log(date); */
+    }
+    date.value = date;
+  }
+}
+
+function maskDateDown(el) {
+  date = el.target.value;
+  if (el.keyCode !== 8) {
+    if (date.length === 2) {
+      date = date.split();
+      date.splice(1, 0, '/');
+      date = date.join('');
+      inputInit.value = date;
+      /* console.log(date); */
+    }else if (date.length === 5) {
+      date = date.split();
+      date.splice(4, 0, '/');
+      date = date.join('');
+      inputInit.value = date;
+      /* console.log(date); */
+    }
+  }
+}
+inputInit.addEventListener('keyup', maskDate);
+inputInit.addEventListener('keydown', maskDateDown)
