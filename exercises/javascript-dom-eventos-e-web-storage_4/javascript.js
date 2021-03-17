1d219aff5e6acacb8c9877a450536186b39e3920
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-17 09:41:44 -0300

    Adicionando configuraçoes de estilo definidas pelo usuario no localstorage

--- exercises/javascript-dom-eventos-e-web-storage_4/javascript.js
+++ exercises/javascript-dom-eventos-e-web-storage_4/javascript.js
@@ -1,36 +1,79 @@
-
-
-    function changeBackground(element) {
-        document.body.style.backgroundColor = element.target.value;
-    }
-    
-    function changeTextColor(element) {
-        document.body.style.color = element.target.value;
-    }
-    
-    function changeFontSize(element) {
-        document.body.style.fontSize = `${element.target.value}px`;
-    }
-    
-    function changeLineHeight(element) {
-        ps = document.getElementsByTagName('p');
-        for (let p of ps) {
-            p.style.lineHeight = `${element.target.value}px`
-        }
-    }
-
-    function changeFontFamily() {
-       /*  alert('oi')
-        let select = document.getElementById('font-family');
-        let value = select.options[select.selectedIndex].value;
-        console.log(value); // pt */
-    }
-
+let styleObj = {
+};
+let p = document.getElementsByTagName('p')[0];
 let inputBackgroundColor = document.getElementById('background-color');
 let inputTextColor = document.getElementById('text-color');
 let inputFontSize = document.getElementById('font-size');
 let inputLineHeight = document.getElementById('line-height');
 let selectFontFamily = document.getElementById('font-family')
+
+function changeBackgroundStorage(value) {
+  document.body.style.backgroundColor = value; 
+}
+function changeTextColorStorage(value) {
+  document.body.style.color = value; 
+}
+function changeFontSizeStorage(value) {
+  p.style.fontSize = value; 
+}
+function changeLineHeightStorage(value) {
+  p.style.lineHeight = value; 
+}
+
+function changeFontFamilyStorage(value) {
+  p.style.fontFamily = value; 
+}
+if (localStorage.getItem('changeBackground')) {
+  changeBackgroundStorage(localStorage.getItem('changeBackground'));
+  inputBackgroundColor.value = localStorage.getItem('changeBackground')
+}
+if (localStorage.getItem('changeTextColor')) {
+  changeTextColorStorage(localStorage.getItem('changeTextColor'));
+  inputTextColor.value = localStorage.getItem('changeTextColor')
+}
+if (localStorage.getItem('changeFontSize')) {
+  changeFontSizeStorage(localStorage.getItem('changeFontSize'));
+  inputFontSize.value = parseInt(localStorage.getItem('changeFontSize'), 10)
+}
+if (localStorage.getItem('changeLineHeight')) {
+  changeLineHeightStorage(localStorage.getItem('changeLineHeight'));
+  inputLineHeight.value = parseInt(localStorage.getItem('changeLineHeight'), 10)
+}
+
+if (localStorage.getItem('changeFonteFamily')) {
+  changeFontFamilyStorage(localStorage.getItem('changeFonteFamily'));
+  selectFontFamily.value = localStorage.getItem('changeFonteFamily');
+}
+
+
+    function changeBackground(element) {
+        document.body.style.backgroundColor = element.target.value;
+        localStorage.setItem('changeBackground',element.target.value);
+    }
+    
+    function changeTextColor(element) {
+        document.body.style.color = element.target.value;
+        localStorage.setItem('changeTextColor',element.target.value);
+    }
+    
+    function changeFontSize(element) {
+      p.style.fontSize = `${element.target.value}px`;
+      localStorage.setItem('changeFontSize',`${element.target.value}px`);
+    }
+    
+    function changeLineHeight(element) {
+      p.style.lineHeight = `${element.target.value}px`
+      localStorage.setItem('changeLineHeight', `${element.target.value}px`)
+    }
+
+    function changeFontFamily() {
+      let select = document.getElementById('font-family');
+      let fontFamily = select.options[select.selectedIndex].value;
+      p.style.fontFamily = fontFamily;
+      localStorage.setItem('changeFonteFamily', fontFamily)
+    }
+
+
 
 //para mudança de cor de background:
 inputBackgroundColor.addEventListener('change', changeBackground);
@@ -41,9 +84,10 @@ inputFontSize.addEventListener('change', changeFontSize);
 
 inputLineHeight.addEventListener('change', changeLineHeight)
 
-//selectFontFamily.addEventListener('change', changeFontFamily)
+selectFontFamily.addEventListener('change', changeFontFamily)
 
-selectFontFamily
+
+
 
 
 
