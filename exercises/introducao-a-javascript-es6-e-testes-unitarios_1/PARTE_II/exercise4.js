9549690b189ad12ab35a33301a4baaa60a12937a
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-24 14:39:11 -0300

    PARTE II, exercise 4, funcao 2 - Criando lista alfabetica das minhas 5 skills

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/PARTE_II/exercise4.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/PARTE_II/exercise4.js
@@ -1,5 +1,23 @@
+/* Função 1: */
 const replaceX = (world) => {
   return `Tryber ${world.replace('x', world)} aqui!`
 }
 
 console.log(replaceX('Vinicius Rocha'));
+
+/* Função 2: */
+let skills = ['Java Script', 'HTML', 'CSS', 'React Native', 'AdonisJS']
+
+const listSkills = (sentence) => {
+  skills = skills.sort();
+  let skillsString = '';
+  for (let skill of skills) {
+    skillsString += 
+    `
+    ${skill}`;
+  }
+  return `${sentence} Minhas cinco principais habilidades são:
+  ${skillsString}`
+}
+
+console.log(listSkills(replaceX('Vinicius Rocha')))
